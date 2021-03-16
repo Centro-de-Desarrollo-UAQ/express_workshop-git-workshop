ff6e77e592996a15f6b1daea9b1b1999fecce2ab
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-03-16 17:51:36 -0600

    added new route ('/:name') in index.js

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 
 app.get("/", (req, res, next) =>{
     res.status(200);
-    res.send("Hola Mundo, bienvenido");
+    res.send("Bienvenido al Servidor");
 });
 
-app.listen(3000, ()=>{
+app.get("/:name", (req, res, next) =>{
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola, " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Server is running...');
 });
